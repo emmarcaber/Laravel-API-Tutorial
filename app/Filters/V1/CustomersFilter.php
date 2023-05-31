26c1cca76ce67486493b6529053d7b4f3fc75093
Emmar Caber <caberemmar@gmail.com>
2023-05-31 10:34:54 +0800

    feat: remove transform method in customers filter

--- app/Filters/V1/CustomersFilter.php
+++ app/Filters/V1/CustomersFilter.php
@@ -28,41 +28,4 @@ class CustomersFilter extends ApiFilter
         'gt' => '>',
         'gte' => '>=',
     ];
-
-    public function transform(Request $request)
-    {
-        $eloQuery = [];
-
-        foreach ($this->safeParms as $parm => $operators) {
-            $query = $request->query($parm);
-
-            // if there is no query set, just skip the iteration
-            if (!isset($query)) {
-                continue;
-            }
-
-            // checks if the parameter is in column map
-            // if in column map, return column map
-            // else return the default parameter
-
-            // null coalescing operator (??)
-            // 0 ?? 42      # returns 9
-            // null ?? 42   $ returns 42
-            $column = $this->columnMap['$parm'] ?? $parm;
-
-            // iterate on all of the operators
-            foreach ($operators as $operator) {
-
-                // if there is value
-                if (isset($query[$operator])) {
-
-                    // eloQuery is congruent to where operator parameters
-                    // [['column', 'operator', 'value']]
-                    $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
-                }
-            }
-        }
-
-        return $eloQuery;
-    }
 }
