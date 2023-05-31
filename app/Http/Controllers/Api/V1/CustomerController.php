2474f424e1e642acba2562c0d4ccd88462ef2679
Emmar Caber <caberemmar@gmail.com>
2023-05-31 15:06:22 +0800

    feat: include related invoices data of customers

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -24,12 +24,15 @@ class CustomerController extends Controller
 
         $filterItems = $filter->transform(request()); // [['columnn', 'operator', 'value']]
 
-        if (count($filterItems) == 0) {
-            return new CustomerCollection(Customer::paginate());
-        } else {
-            $customers = Customer::where($filterItems)->paginate();
-            return new CustomerCollection($customers->appends(request()->query()));
+        $includeInvoices = request()->query('includeInvoices');
+
+        $customers = Customer::where($filterItems);
+
+        if ($includeInvoices) {
+            $customers = $customers->with('invoices');
         }
+
+        return new CustomerCollection($customers->paginate()->appends(request()->query()));
     }
 
     /**
